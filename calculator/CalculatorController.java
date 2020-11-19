f66364385dbd65ec61f1c4c44da9eebfcd157868
SUHOPARK1 <qkrtnghsla@naver.com>
2020-11-19 10:49:16 +0900

    first commit

---
+++ calculator/CalculatorController.java
@@ -0,0 +1,38 @@
+package calculator;
+
+import user.*;
+import bmi.*;
+public class CalculatorController {
+	
+	public int execute(CalculatorDTO calculator) {
+		CalculatorService service = new CalculatorServiceImpl();
+		int res = 0;
+		String op = calculator.getOpcode();
+		if(op.equals("+")) {
+			res = service.add(calculator);
+		}else if(op.equals("-")){
+			res = service.minus(calculator);
+		}else if(op.equals("*")){
+			res = service.multi(calculator);
+		}else if(op.equals("/")){
+			res = service.divid(calculator);
+		}else if(op.equals("%")){
+			res = service.modul(calculator);
+		}
+		return res;
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
